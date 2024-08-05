b0f8019f92a2f68464a127b11ea8dc7cd9361120
Abhinav <abhinavchaudhary2014@gmail.com>
2024-08-05 21:40:33 +0530

    Fixd bug

--- bot.js
+++ bot.js
@@ -23,7 +23,7 @@ const express = require('express');
 const app = express();
 
 app.listen(4000, () => {
-	console.log(`Listening on port ${port}`);
+	console.log(`Listening on port 4000`);
 });
 
 //*******************code to deploy on render ends here*************************
